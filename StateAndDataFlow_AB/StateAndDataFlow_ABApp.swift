857058289231c065ccd85f2ad2d9b7229af533b1
Anton Boev <110462969+t0H1k@users.noreply.github.com>
2022-12-17 17:33:16 +0300

    Initial Commit

---
+++ StateAndDataFlow_AB/StateAndDataFlow_ABApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlow_ABApp.swift
+//  StateAndDataFlow_AB
+//
+//  Created by Anton Boev on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlow_ABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
